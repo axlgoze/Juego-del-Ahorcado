b34cab512b08f4ecb0c2c088047657cfac2035b5
Axel Reyes <rbaxel.f@gmail.com>
2022-06-20 23:49:32 -0500

    dibujando primer linea para letra

--- js/dibujaCanvas1.js
+++ js/dibujaCanvas1.js
@@ -1,5 +1,18 @@
+// canvas para el juego
+
 var pantalla = document.querySelector("canvas");
 var pincel = pantalla.getContext("2d");
 
 pincel.fillStyle = "lightgrey";
-pincel.fillRect(0,0,1200,800);
+pincel.fillRect(0,0,1200,430);
+
+// function dibujaBarras(){
+    pincel.fillStyle = "gold";
+    pincel.fillRect(150,350,90,10);
+    
+
+
+    // pincel.fill();
+// }
+    
+// dibujaBarras();
