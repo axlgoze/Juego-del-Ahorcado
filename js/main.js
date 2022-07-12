0edc73d6fe2d24669cb318985c668a968ae1530c
Axel Reyes <rbaxel.f@gmail.com>
2022-07-12 12:12:45 -0500

    agregando funcionalidad para agregar palabras al arreglo de palabras

---
+++ js/main.js
@@ -0,0 +1,17 @@
+// funcion para elegir palabras secreta
+var words = ["arbol","cereza","cuchillo"];
+console.log(words.length);
+
+var entrada=document.querySelector(".nuevaEntrada");
+
+
+// funcion para agregar una palabra al arreglo de palabras a mostrar
+function addWord(newWord){
+    newWord = entrada.value;
+    words.push(newWord);
+    for(i=0;i<words.length;i++){
+        console.log(words[i]);
+    }
+}
+
+//addWord(sol);
