320b422df483c22d4471abcba8df5cfcaa85bae1
Axel Reyes <rbaxel.f@gmail.com>
2022-07-12 13:15:49 -0500

    agregando validacion sencilla para entradas con números

--- js/main.js
+++ js/main.js
@@ -8,13 +8,22 @@ var entrada=document.querySelector(".nuevaEntrada");
 // funcion para agregar una palabra al arreglo de palabras a mostrar
 function addWord(newWord){
     newWord = entrada.value;
-    words.push(newWord);
-    for(i=0;i<words.length;i++){
-        console.log(words[i]);
+    if(newWord==""){
+        alert("Ingresa una palabra");
+        entrada.focus();
+    }else if(!isNaN(newWord)){
+        alert("Ingresa una palabra valida (Sin números)");
+        document.getElementById("nuevaEntrada").value="";
+        entrada.focus();
+    }else{
+        words.push(newWord);
+        for(i=0;i<words.length;i++){
+            console.log(words[i]);
+        }
+        document.getElementById("nuevaEntrada").value="";
+        alert("Palabra agregada correctamente");
+        entrada.focus();
     }
-    document.getElementById("nuevaEntrada").value="";
-    alert("Palabra agregada correctamente");
-
 }
 
 //addWord(sol);
